12303aed5f8d34c687658d3782ea7c17607230b6
chandram13 <mschandr@uci.edu>
2022-09-14 11:28:28 -0700

    Initial commit

---
+++ src/main/scala/Main.scala
@@ -0,0 +1,6 @@
+class diagnosingskindisorders{
+  def skinPrognosis(symptom):
+    if symptom == "red spots":
+      println("These red spots are located often on your face, neck, shoulders, chest, and upper bck. This is acne.")
+    if symptom == ""
+}
